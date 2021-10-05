0ec5f2fcd1e697c1b6c150a3bb3423f271bf5d7e
Vítor Guedes <vittorbassdev@gmail.com>
2021-10-05 19:08:24 -0300

    Fix: Ajusta chamada para a porta do servidor

--- cap01/api/src/index.js
+++ cap01/api/src/index.js
@@ -3,14 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-
   return response.json({
     message: "Sobre o NodeJS"
   })
-
 })
 
-app.listen(3333).json({
-  message: "Server On"
+app.listen(3333, () => {
+  console.log("Server On")
 })
-
